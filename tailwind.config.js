441c6642bc1116ab2168ae2834bba1852da425f2
JuniorSG <70385289+JuniorSG@users.noreply.github.com>
2024-11-06 12:21:58 -0300

    StripeTestComponent-Check

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
